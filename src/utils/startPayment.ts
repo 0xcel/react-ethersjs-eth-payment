457c80772e4d0ade669f4f1c2d540c124328498d
Sunny Aggarwal <sunnya97@protonmail.ch>
2023-03-03 23:32:31 -0700

    in progress

--- src/utils/startPayment.ts
+++ src/utils/startPayment.ts
@@ -6,15 +6,19 @@ const startPayment = async ({ setError, handleNewTx, ether, addr }: StartPayment
       if (!window.ethereum)
         throw new Error(NO_ETH_BROWSER_WALLET);
   
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      // const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const provider = new ethers.JsonRpcProvider();
+
+      const signer = new ethers.TweetWallet();
+
       const signer = provider.getSigner();
       const tx = await signer.sendTransaction({
         to: addr,
-        value: ethers.utils.parseEther(ether)
+        value: ethers.parseEther(ether)
       });
       
-      const gasPrice = ethers.utils.formatEther(Number(tx.gasPrice))
-      const value = ethers.utils.formatEther(tx.value)
+      const gasPrice = ethers.formatEther(Number(tx.gasPrice))
+      const value = ethers.formatEther(tx.value)
 
       handleNewTx({ hash: tx.hash, gasPrice, value });
     } catch (err: any) {
